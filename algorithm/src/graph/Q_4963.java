f1796f9198a023f8c93f1bbb261da28e2c403b88
djeu1116 <rladbgus781@gmail.com>
2025-12-23 21:08:12 +0900

    Q4963

---
+++ algorithm/src/graph/Q_4963.java
@@ -0,0 +1,62 @@
+package graph;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Q_4963 {
+
+    static int[][] map;
+    static boolean[][] visited;
+    static int w, h;
+    static int[] dx =  {-1, 1, 0, 0, 1, 1, -1, -1};
+    static int[] dy = {0, 0, -1, 1, -1, 1, 1, -1};
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+
+        while (true) {
+            st = new StringTokenizer(br.readLine(), " ");
+            w = Integer.parseInt(st.nextToken());
+            h = Integer.parseInt(st.nextToken());
+            if (w == 0 && h == 0) break;
+            map = new int[w][h];
+            visited = new boolean[w][h];
+
+            for (int i = 0; i < h; i++) {
+                st = new  StringTokenizer(br.readLine(), " ");
+                for (int j = 0; j < w; j++) {
+                    map[j][i] =  Integer.parseInt(st.nextToken());
+                }
+            }
+            int island = 0;
+            for (int i = 0; i < w; i++) {
+                for (int j = 0; j < h; j++) {
+                    if (map[i][j] == 1 && !visited[i][j]) {
+                        island++;
+                        dfs(i, j);
+                    }
+                }
+            }
+            System.out.println(island);
+        }
+        br.close();
+    }
+
+    public static void dfs(int x, int y) {
+        visited[x][y] = true;
+        for (int i = 0; i < dx.length; i++) {
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+
+            if (nx >= 0 && ny >= 0 && nx < w && ny < h) {
+                if (map[nx][ny] == 1 && !visited[nx][ny]) {
+                    dfs(nx, ny);
+                }
+            }
+        }
+    }
+}
