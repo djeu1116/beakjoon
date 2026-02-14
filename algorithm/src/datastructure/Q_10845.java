cf84fac33b1148cfc7aa5cca19f3f0a6d16950d7
djeu1116 <rladbgus781@gmail.com>
2026-02-14 20:13:40 +0900

    Q10845

---
+++ algorithm/src/datastructure/Q_10845.java
@@ -0,0 +1,43 @@
+package datastructure;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.StringTokenizer;
+
+public class Q_10845 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        Deque<Integer> q = new LinkedList<>();
+
+        for (int i = 0; i < n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            String command = st.nextToken();
+
+            if (command.contains("push")) {
+                int value = Integer.parseInt(st.nextToken());
+                q.add(value);
+            } else if (command.contains("pop")) {
+                if (q.isEmpty()) System.out.println(-1);
+                else System.out.println(q.poll());
+            } else if (command.contains("size")) {
+                System.out.println(q.size());
+            } else if (command.contains("empty")) {
+                if (q.isEmpty()) System.out.println(1);
+                else System.out.println(0);
+            } else if (command.contains("front")) {
+                if (q.isEmpty()) System.out.println(-1);
+                else System.out.println(q.peek());
+            } else if (command.contains("back")) {
+                if (q.isEmpty()) System.out.println(-1);
+                else System.out.println(q.peekLast());
+            }
+        }
+
+
+
+    }
+}
