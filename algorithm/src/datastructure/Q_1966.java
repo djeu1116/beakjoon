054e55d7fcee95d98c76453e3227e3f2905d931c
Yuhyun <rladbgus781@gmail.com>
2026-03-20 15:15:41 +0900

    Q1966

--- algorithm/src/datastructure/Q_1966.java
+++ algorithm/src/datastructure/Q_1966.java
@@ -6,63 +6,43 @@ import java.io.InputStreamReader;
 import java.util.*;
 
 public class Q_1966 {
-
-    static LinkedList<int[]> queue;
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
         StringTokenizer st;
+        int T = Integer.parseInt(br.readLine());
 
-        int t = Integer.parseInt(br.readLine());
-        while (t --> 0) {
-            queue = new LinkedList<>();
-
+        for (int i = 0; i < T; i++) {
+            int result = 0;
+            Queue<int[]> queue = new LinkedList<int[]>();
             st = new StringTokenizer(br.readLine());
-
-            int n = Integer.parseInt(st.nextToken());
-            int m = Integer.parseInt(st.nextToken());
-
+            int N = Integer.parseInt(st.nextToken());
+            int M = Integer.parseInt(st.nextToken());
             st = new StringTokenizer(br.readLine());
-
-            for (int i = 0; i < n; i++) {
-                // {초기 위치, 중요도}
-                queue.add(new int[]{i, Integer.parseInt(st.nextToken())});
+            for (int j = 0; j < N; j++) {
+                queue.add(new int[] {j, Integer.parseInt(st.nextToken())});
             }
 
-            int cnt = 0;
+            while (true) {
+                int[] cur = queue.poll();
+                boolean check = true;
 
-            while (!queue.isEmpty()) {
-                int[] front = queue.poll();
-                boolean isMax = true;
-
-                for (int i = 0; i < queue.size(); i++) {
-                    if (front[1] < queue.get(i)[1]) {
-
-                        queue.offer(front);
-                        for (int j = 0; j < i; j++) {
-                            queue.offer(queue.poll());
-                        }
-                        isMax = false;
+                for (int[] curQ : queue) {
+                    if (curQ[1] > cur[1]) {
+                        check = false;
                         break;
                     }
                 }
 
-                if (isMax == false) {
-                    continue;
+                if (check) {
+                    result++;
+                    if (cur[0] == M) break;
                 }
-
-                cnt ++;
-                if (front[0] == m) {
-                    break;
+                else {
+                    queue.add(cur);
                 }
             }
-
-            sb.append(cnt).append("\n");
-
+            System.out.println(result);
         }
-
-        System.out.println(sb);
-
+        br.close();
     }
 }
